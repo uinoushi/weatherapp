27f01c0ff2ff68a4adc84f45426ae17e3223212b
Noushad Kottilingal <noushadkottilingal@ind-mac-0003.local>
2020-07-15 13:25:06 +0530

    Updates

---
+++ precache-manifest.e67efeb945f5c218fbc19d0a3d6d3a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348cadce7dff59e0ca32a818cb4b3b8d",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "e3769a6c90d6cdecb935",
+    "url": "/weatherapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0eb649d8cd5d92ab473f",
+    "url": "/weatherapp/static/js/2.c89f289e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weatherapp/static/js/2.c89f289e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3769a6c90d6cdecb935",
+    "url": "/weatherapp/static/js/main.60400bbb.chunk.js"
+  },
+  {
+    "revision": "ff907cfbbb256dae02f8",
+    "url": "/weatherapp/static/js/runtime-main.f16cac52.js"
+  }
+]);
